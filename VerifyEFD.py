92fcdb470f2746444d1cbb454030d93bf681a358
Christopher Contaxis <CContaxis@lsst.org>
2018-04-04 11:34:33 -0700

    EFD Bug Fix

--- VerifyEFD.py
+++ VerifyEFD.py
@@ -20,7 +20,7 @@ class VerifyEFD:
         cur = db.cursor()
         
         # Issue a command to generate a command, event, and telemetry
-        m1m3.Start("Default", False)
+        m1m3.Start("Default")
         
         # Check SAL Event
         result, data = m1m3.GetEventDetailedState()
@@ -28,16 +28,18 @@ class VerifyEFD:
         Equal("SAL m1m3_logevent_DetailedState.DetailedState", data.DetailedState, m1m3_shared_DetailedStates_DisabledState)
         
         # Check EFD Command
-        cur.execute("SELECT Start, SettingsToApply FROM m1m3_command_Start ORDER BY date_time DESC LIMIT 1")
-        row = cur.fetchone()
-        Equal("EFD m1m3_command_Start.Start", int(row[0]), 1)
-        Equal("EFD m1m3_command_Start.SettingsToApply", row[1], "Default")
+        count = cur.execute("SELECT Start, SettingsToApply FROM m1m3_command_Start ORDER BY date_time DESC LIMIT 1")
+        if (Equal("EFD m1m3_command_Start Count", count, 1)):
+            row = cur.fetchone()
+            Equal("EFD m1m3_command_Start.Start", int(row[0]), 1)
+            Equal("EFD m1m3_command_Start.SettingsToApply", row[1], "Default")
         
         # Check EFD Event
-        cur.execute("SELECT Timestamp, DetailedState FROM m1m3_logevent_DetailedState WHERE Timestamp <= %0.3f ORDER BY Timestamp DESC LIMT 1" % (eventTimestamp + 0.001))
-        row = cur.fetchone()
-        InTolerance("EFD m1m3_logevent_DetailedState.Timestamp", float(row[0]), data.Timestamp, 0.001)
-        Equal("EFD m1m3_logevent_DetailedState.DetailedState", int(row[1]), data.DetailedState)
+        count = cur.execute("SELECT Timestamp, DetailedState FROM m1m3_logevent_DetailedState WHERE Timestamp <= %0.3f ORDER BY Timestamp DESC LIMIT 1" % (eventTimestamp + 0.001))
+        if (Equal("EFD m1m3_logevent_DetailedState Count", count, 1)):
+            row = cur.fetchone()
+            InTolerance("EFD m1m3_logevent_DetailedState.Timestamp", float(row[0]), data.Timestamp, 0.001)
+            Equal("EFD m1m3_logevent_DetailedState.DetailedState", int(row[1]), data.DetailedState)
                 
         # Check SAL Telemetry
         result, data = m1m3.GetSampleInclinometerData()
@@ -45,10 +47,11 @@ class VerifyEFD:
         GreaterThan("SAL m1m3_InclinometerData.Timestamp", data.Timestamp, eventTimestamp)
         
         # Check EFD Telemetry
-        cur.execute("SELECT Timestamp, InclinometerAngle FROM m1m3_InclinometerData WHERE Timestamp <= %0.3f ORDER BY Timestamp DESC LIMIT 1" % (telemetryTimestamp + 0.001))
-        row = cur.fetchone()
-        InTolerance("EFD m1m3_InclinometerData.Timestamp", float(row[0]), data.Timestamp, 0.001)
-        InTolerance("EFD m1m3_InclinometerData.InclinometerAngle", float(row[1]), data.InclinometerAngle, 0.001)
+        count = cur.execute("SELECT Timestamp, InclinometerAngle FROM m1m3_InclinometerData WHERE Timestamp <= %0.3f ORDER BY Timestamp DESC LIMIT 1" % (telemetryTimestamp + 0.001))
+        if (Equal("EFD m1m3_InclinometerData Count", count, 1)):
+            row = cur.fetchone()
+            InTolerance("EFD m1m3_InclinometerData.Timestamp", float(row[0]), data.Timestamp, 0.001)
+            InTolerance("EFD m1m3_InclinometerData.InclinometerAngle", float(row[1]), data.InclinometerAngle, 0.001)
         
         # Get back into StandbyState
         m1m3.Standby()
