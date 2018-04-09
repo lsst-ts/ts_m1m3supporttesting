2da8c93a792e7dba4a3fe1c1800675b5a069eaad
Christopher Contaxis <CContaxis@lsst.org>
2018-04-09 09:38:49 -0700

    Fix state change bug with RaiseM1M3.

--- RunAll.py
+++ RunAll.py
@@ -1,5 +1,6 @@
 import M1M3
 import CellSimulator
+import VerifyAccelerometer
 import VerifyEFD
 import VerifyStateChanges
 from HardpointActuatorTable import *
@@ -56,5 +57,5 @@ for row in forceActuatorTable:
 
 VerifyEFD.VerifyEFD().Run(m1m3, sim)
 VerifyStateChanges.VerifyStateChanges().Run(m1m3, sim)
-
+VerifyAccelerometer.VerifyAccelerometer().Run(m1m3, sim)
        
