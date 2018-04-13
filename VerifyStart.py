ec04cc09561eb063b42bb17c3fd67a5031af5296
Christopher Contaxis <CContaxis@lsst.org>
2018-04-13 16:24:37 -0700

    Fix start test.

--- VerifyStart.py
+++ VerifyStart.py
@@ -8,24 +8,24 @@ from SALPY_m1m3 import *
 # Description:  Verify start command many times
 ########################################################################
 
-class VerifyForceActuators:
+class VerifyStart:
     def Run(self, m1m3, sim):
         Header("Verify Start")
-        bad = False
-        while not bad:
+        ok = True
+        while ok:
             m1m3.Start("Default")
             result, data = m1m3.GetEventDetailedState()
-            bad = bad || Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_DisabledState)
+            ok = ok and Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_DisabledState)
             result, data = m1m3.GetEventSummaryState()
-            bad = bad || Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_DisabledState)
+            ok = ok and Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_DisabledState)
             time.sleep(1)
-            if bad:
+            if not ok:
                 Log("Start Transition")
             m1m3.Standby()
             result, data = m1m3.GetEventDetailedState()
-            bad = bad || Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_StandbyState)
+            ok = ok and Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_StandbyState)
             result, data = m1m3.GetEventSummaryState()
-            bad = bad || Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_StandbyState)
+            ok = ok and Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_StandbyState)
             time.sleep(1)
-            if bad:
+            if not ok:
                 Log("Standby Transition")
