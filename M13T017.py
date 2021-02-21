e724f9dd96ae746b467cd3e7256c0a77b93cbde3
Petr Kubánek <pkubanek@lsst.org>
2021-02-21 18:01:54 +0000

    Update M13T017

--- M13T017.py
+++ M13T017.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3.8
+
+# This file is part of M1M3 SS test suite.
+#
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 ########################################################################
 # Test Numbers: M13T-017
 # Author:       CContaxis
@@ -8,7 +31,7 @@
 # - Apply a set of active optic forces
 # - Verify the active optic forces are applied
 # - Clear active optic forces
-# - Verify the active optic forces are no longer applied 
+# - Verify the active optic forces are no longer applied
 # - Apply an active optic force by bending modes
 # - Verify the active optic forces are applied
 # - Clear active optic forces
@@ -22,138 +45,102 @@
 # - Transition from active engineering state to standby
 ########################################################################
 
-import time
-import math
-from Utilities import *
-from SALPY_m1m3 import *
-from ForceActuatorTable import *
-from HardpointActuatorTable import *
-from Setup import *
 from CalculateBendingModeForces import *
+from MTM1M3Test import *
 
-TEST_MODE = [0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+from lsst.ts.idl.enums import MTM1M3
+
+TEST_MODE = [
+    0.0,
+    0.0,
+    0.0,
+    0.5,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+]
 TEST_FORCE = [1.2] * 156
 TEST_TOLERANCE = 0.1
 WAIT_UNTIL_TIMEOUT = 600
 
-class M13T017:
-    def Run(self, m1m3, sim, efd):
-        Header("M13T-017: Active Optic Actuator Force Updates")
-        
-        # Transition to disabled state
-        m1m3.Start("Default")
-        result, data = m1m3.GetEventDetailedState()
-        Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_DisabledState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_DisabledState)
-        
-        # Transition to parked state
-        m1m3.Enable()
-        result, data = m1m3.GetEventDetailedState()
-        Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_ParkedState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_EnabledState)
-        
-        # Transition to parked engineering state
-        m1m3.EnterEngineering()
-        result, data = m1m3.GetEventDetailedState()
-        Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_ParkedEngineeringState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_EnabledState)
-        
-        # Raise mirror (therefore entering the Raised Engineering State).
-        m1m3.RaiseM1M3(False)
-        result, data = m1m3.GetEventDetailedState()
-        Equal("SAL m1m3_logevent_DetailedState.DetailedState", data.DetailedState, m1m3_shared_DetailedStates_RaisingEngineeringState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_EnabledState)
-        
-        # Wait until active engineering state
-        WaitUntil("DetailedState", WAIT_UNTIL_TIMEOUT, lambda: m1m3.GetEventDetailedState()[1].DetailedState == m1m3_shared_DetailedStates_ActiveEngineeringState)
-        
-        time.sleep(5.0)
-        
+
+class M13T017(MTM1M3Test):
+    async def test_active_optics_updates(self):
+        self.printHeader("M13T-017: Active Optic Actuator Force Updates")
+
+        await self.startup(MTM1M3.DetailedState.ACTIVEENGINEERING)
+
+        await asyncio.sleep(5.0)
+
+        def verifyForces(zForces):
+            # Get active optic forces
+            data = self.m1m3.evt_appliedActiveOpticForces.get()
+
+            # Validate all Z data
+            self.assertListAlmostEqual(
+                data.zForces,
+                zForces,
+                delta=TEST_TOLERANCE,
+                msg=f"Verifying applied active optics forces",
+            )
+
+        async def clear_and_verify():
+            await self.m1m3.cmd_clearActiveOpticForces.start()
+            await asyncio.sleep(1.0)
+            self.verifyForces([0] * 156)
+            await asyncio.sleep(1.0)
+
         # Clear active optic forces and verify
-        m1m3.ClearActiveOpticForces()
-        time.sleep(1.0)
-        self.verifyForces("Clear AO Force", m1m3, [0] * 156)
-        time.sleep(1.0)
-        
+        await clear_and_verify()
+
         # Apply active optic force and verify
-        m1m3.ApplyActiveOpticForces(TEST_FORCE)
-        time.sleep(1.0)
-        self.verifyForces("Apply AO Force by Forces", m1m3, TEST_FORCE)
-        time.sleep(1.0)
-        
-        # Clear active optic forces and verify
-        m1m3.ClearActiveOpticForces()
-        time.sleep(1.0)
-        self.verifyForces("Clear AO Force", m1m3, [0] * 156)
-        time.sleep(1.0)
-        
-        # Apply active optic force by bending mode and verify
-        m1m3.ApplyActiveOpticForcesByBendingModes(TEST_MODE)
-        time.sleep(1.0)
-        self.verifyForces("Apply AO Force by Forces", m1m3, CalculateBendingModeForces(TEST_MODE))
-        time.sleep(1.0)
-        
-        # Clear active optic forces and verify
-        m1m3.ClearActiveOpticForces()
-        time.sleep(1.0)
-        self.verifyForces("Clear AO Force", m1m3, [0] * 156)
-        time.sleep(1.0)
-        
-        # Apply active optic force by bending mode and verify
-        m1m3.ApplyActiveOpticForcesByBendingModes(TEST_MODE)
-        time.sleep(1.0)
-        self.verifyForces("Apply AO Force by Forces", m1m3, CalculateBendingModeForces(TEST_MODE))
-        time.sleep(1.0)
-        
+        await self.m1m3.cmd_applyActiveOpticForces.set_start(
+            zForces=TEST_FORCE, xForces=[0] * 6, yForces=[0] * 100
+        )
+        await asyncio.sleep(1.0)
+        self.verifyForces(TEST_FORCE)
+        await asyncio.sleep(1.0)
+
+        for i in range(2):
+            # Clear active optic forces and verify
+            await clear_and_verify()
+
+            # Apply active optic force by bending mode and verify
+            await self.m1m3.cmd_applyActiveOpticForcesByBendingModes.set_start(
+                coefficients=TEST_MODE
+            )
+            await asyncio.sleep(1.0)
+            self.verifyForces(CalculateBendingModeForces(TEST_MODE))
+            await asyncio.sleep(1.0)
+
         # Apply active optic force and verify
-        m1m3.ApplyActiveOpticForces(TEST_FORCE)
-        time.sleep(1.0)
-        self.verifyForces("Apply AO Force by Forces", m1m3, TEST_FORCE)
-        time.sleep(1.0)
-        
+        await self.m1m3.cmd_applyActiveOpticForces.set_start(
+            zForces=TEST_FORCE, xForces=[0] * 6, yForces=[0] * 100
+        )
+        await asyncio.sleep(1.0)
+        self.verifyForces(TEST_FORCE)
+        await asyncio.sleep(1.0)
+
         # Clear active optic forces and verify
-        m1m3.ClearActiveOpticForces()
-        time.sleep(1.0)
-        self.verifyForces("Clear AO Force", m1m3, [0] * 156)
-        time.sleep(1.0)
-        
-        # Transition to lowering engineering state
-        m1m3.LowerM1M3()
-        result, data = m1m3.GetEventDetailedState()
-        Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_LoweringEngineeringState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_EnabledState)
-        
-        # Wait until parked engineering state
-        WaitUntil("DetailedState", WAIT_UNTIL_TIMEOUT, lambda: m1m3.GetEventDetailedState()[1].DetailedState == m1m3_shared_DetailedStates_ParkedEngineeringState)
-            
-        # Transition to disabled state
-        m1m3.Disable()
-        result, data = m1m3.GetEventDetailedState()
-        Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_DisabledState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_DisabledState)
-        
-        # Transition to standby state
-        m1m3.Standby()
-        result, data = m1m3.GetEventDetailedState()
-        Equal("DetailedState", data.DetailedState, m1m3_shared_DetailedStates_StandbyState)
-        result, data = m1m3.GetEventSummaryState()
-        Equal("SummaryState", data.SummaryState, m1m3_shared_SummaryStates_StandbyState)
-        
-    def verifyForces(self, preText, m1m3, zForces):
-        # Get active optic forces
-        result, data = m1m3.GetEventAppliedActiveOpticForces()
-        
-        # Validate all Z data
-        for i in range(156):
-            InTolerance("%s m1m3_logevent_AppliedActiveOpticForces.ZForces[%d]" % (preText, i), data.ZForces[i], zForces[i], TEST_TOLERANCE)
-        
+        await clear_and_verify()
+
+        await self.shutdown(MTM1M3.DetailedState.STANDBY)
+
+
 if __name__ == "__main__":
-    m1m3, sim, efd = Setup()
-    M13T017().Run(m1m3, sim, efd)
-    Shutdown(m1m3, sim, efd)
+    asynctest.main()
